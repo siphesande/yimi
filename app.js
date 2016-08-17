e9d9ca76bc7549e77e79da39e4c9fa11566e6053
MsEmma <emmaphiri@hotmail.com>
2016-08-17 12:04:35 +0200

    initial commits

---
+++ app.js
@@ -0,0 +1,50 @@
+// 'use strict'
+
+var express = require('express'),
+    exphbs = require('express-handlebars'),
+    app = express(),
+    mysql = require('mysql'),
+    myConnection = require('express-myconnection'),
+    bodyParser = require('body-parser');
+
+var dbOptions = {
+    host: 'localhost',
+    user: 'root',
+    password: 'yimi',
+    port: 3306,
+    database: 'yimi'
+};
+
+app.set('port', (process.env.PORT || 3000));
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+//setup middleware
+app.use(myConnection(mysql, dbOptions, 'single'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+
+function errorHandler(err, req, res, next) {
+    res.status(500);
+    res.render('error', {
+        error: err
+    });
+}
+
+
+app.use(errorHandler);
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
